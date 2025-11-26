52c7d84d47d25bc4cc4f77b0b71b8fcc7bd79b49
Hamdi <hs2086@fayoum.edu.eg>
2025-11-26 17:50:07 +0200

    Initialize Onion Architecture structure

---
+++ E-Commerce/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+
+
+var app = builder.Build();
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
